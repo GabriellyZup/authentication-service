fe885ce657e59ed253c626a075ad4e410c36abf4
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-02-26 17:33:07 -0300

    feat:  create User and Role entities
    create user and role models to study

--- src/main/java/com/zup/authentication_service/models/User.java
+++ src/main/java/com/zup/authentication_service/models/User.java
@@ -1,4 +1,35 @@
 package com.zup.authentication_service.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }

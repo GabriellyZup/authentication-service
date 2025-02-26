d1ea7a5cab373982f5ef86d287b71a2374206e25
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-02-26 17:43:19 -0300

    feat: configure Spring Security with basic authentication
    criate sec config class
    basic endpoint config to test
    basic authenticatrion  "willl bem subst to jw" early

--- src/main/java/com/zup/authentication_service/infra/SecurityConfig.java
+++ src/main/java/com/zup/authentication_service/infra/SecurityConfig.java
@@ -1,4 +1,42 @@
 package com.zup.authentication_service.infra;
 
+package com.zup.authentication.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(); // Temporário, será substituído por JWT
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, CustomUserDetailsService userDetailsService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
 }
